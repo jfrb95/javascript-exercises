8db3a9fee5aa3c0feac1e479e8f222be31b34ee4
Jako <43934830+jfrb95@users.noreply.github.com>
2024-08-03 11:28:31 +0100

    Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (typeof num1 !== 'number' ||
+        typeof num2 !== 'number' ||
+        num1 < 0 ||
+        num2 < 0 ||
+        num1 % 1 !== 0 ||
+        num2 % 1 !== 0) {
+            return "ERROR"
+    } else {
+        return (Math.abs(num1 - num2) + 1) / 2 * (num1 + num2);
+    }
 };
 
 // Do not edit below this line
