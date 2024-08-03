2b978927c9a6a7073a8307b44f162f6b05086a94
Jako <43934830+jfrb95@users.noreply.github.com>
2024-08-03 16:41:36 +0100

    Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  const inCelsius = (temperatureInFahrenheit - 32) * (5 / 9);
+  return Math.round(inCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  const inFahrenheit = temperatureInCelsius * 9 / 5 + 32;
+  return Math.round(inFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
