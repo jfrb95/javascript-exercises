804fecc452de564fc6673cbf05d0dc0dee9b5375
Jako <43934830+jfrb95@users.noreply.github.com>
2024-08-03 11:13:51 +0100

    Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    for (const value of values) {
+        for (let i = 0; i < array.length; array[i] === value ? i : ++i){
+            if (array[i] === value){
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
