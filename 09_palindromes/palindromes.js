00673f5f69c5854c63d310000b7bb5de18769cb1
Jako <43934830+jfrb95@users.noreply.github.com>
2024-08-12 18:47:36 +0100

    complted

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const acceptedCharacters = "0123456789abcdefghijklmnopqrstuvwxyz";
+    const strippedStr = str.toLowerCase().split("").filter((element) => {
+        return acceptedCharacters.includes(element)
+    })
+    .join("");
 
+    let leftPointer = 0;
+    let rightPointer = strippedStr.length - 1;
+    while (leftPointer < rightPointer) {
+        if (strippedStr[leftPointer] !== strippedStr[rightPointer]) {
+            return false
+        }
+        leftPointer += 1;
+        rightPointer -= 1;
+    }
+
+    return true
 };
 
 // Do not edit below this line
