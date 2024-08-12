9046ce0e8605bdc53aae82816826c39e50044df8
Jako <43934830+jfrb95@users.noreply.github.com>
2024-08-12 19:02:54 +0100

    Completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.reduce((acc, curr) => {
+        return [...acc, curr.title]
+    }, [])
 };
 
 // Do not edit below this line
