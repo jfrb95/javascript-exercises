e6324727da49877b5fcfa0c8bc8b2782e7a33dbc
Jako <43934830+jfrb95@users.noreply.github.com>
2024-08-12 19:17:37 +0100

    Completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
+    return arr.reduce((acc, curr) => {
+        if ((acc.yearOfDeath ? acc.yearOfDeath : currentYear) - acc.yearOfBirth > (curr.yearOfDeath ? curr.yearOfDeath : currentYear) - curr.yearOfBirth) {
+            return acc
+        } else {
+            return curr
+        }
+    })
 };
 
 // Do not edit below this line
