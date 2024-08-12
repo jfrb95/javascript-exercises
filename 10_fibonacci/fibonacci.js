5d702d63a9466e39108c878357bd1883d51b0243
Jako <43934830+jfrb95@users.noreply.github.com>
2024-08-12 19:01:04 +0100

    Completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (+num < 0) {
+        return "OOPS"
+    } else if (+num === 0) {
+        return 0
+    }
+    let leftPointer = 1;
+    let rightPointer = 1;
+    for (let i = 2; i < +num; ++i) {
+        const temp = rightPointer;
+        rightPointer += leftPointer;
+        leftPointer = temp;
+    }
+    return rightPointer
 };
 
 // Do not edit below this line
